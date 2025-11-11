a7f1d69466d5515d6a954fea2bf2546d4518c4c1
Rozan Laudzai <rozanlaudzai717@gmail.com>
2025-11-12 00:54:48 +0700

    add Lick function

--- advocate.go
+++ advocate.go
@@ -5,3 +5,7 @@ import "fmt"
 func ExplainFruit(fruit Fruit) string {
 	return fmt.Sprintf("This fruit named %v, has %v of acidity", fruit.Name, fruit.Acidity)
 }
+
+func Lick(fruit Fruit) string {
+	return fmt.Sprintf("%v is very good!", fruit.Name)
+}
