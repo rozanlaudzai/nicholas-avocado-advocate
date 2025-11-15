7db82ca972d24ae78e4601b2c1fa2b94b77899fe
Rozan Laudzai <rozanlaudzai717@gmail.com>
2025-11-15 08:43:12 +0700

    fix: Pay func in payment.go

--- payment/payment.go
+++ payment/payment.go
@@ -8,5 +8,5 @@ import (
 
 func Pay(fruit nicholasavocadoadvocate.Fruit) (bool, string) {
 	// doc aja sih ini
-	return false, fmt.Sprintf("You successfully purchased a %v with %v acidity.", fruit.Name, fruit.Acidity)
+	return true, fmt.Sprintf("You successfully purchased a %v with %v acidity.", fruit.Name, fruit.Acidity)
 }
